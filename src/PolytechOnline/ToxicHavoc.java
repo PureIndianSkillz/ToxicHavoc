07cd3b285b058b3c248a9cb925a1015c8de9a3e1
PureIndianSkillz <patelravv17@yahoo.com>
2015-01-08 13:16:24 -0500

    Updates-Spawn

--- src/PolytechOnline/ToxicHavoc.java
+++ src/PolytechOnline/ToxicHavoc.java
@@ -3,13 +3,17 @@ package PolytechOnline;
 
 
 import java.util.logging.Logger;
+
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.Location;
+import org.bukkit.World;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
+
 import PolytechOnline.threads.StartCountdown;
 
 public class ToxicHavoc extends JavaPlugin{ 
@@ -35,6 +39,10 @@ public class ToxicHavoc extends JavaPlugin{
 	   if(commandLabel.equalsIgnoreCase("setteamspawn")){
 		   player.sendMessage(ChatColor.RED + "This command has not been added yet!");
 	   }
+	   if(commandLabel.equalsIgnoreCase("spawn")){
+		   World world = player.getWorld();
+			Location loc = new Location(world,571, 49, -734, 89, 0);
+			player.teleport(loc);}
 			
 			
 			
@@ -45,8 +53,7 @@ public class ToxicHavoc extends JavaPlugin{
 			
 			
 			
-			
-			
+			{}
 			
 			
 			
