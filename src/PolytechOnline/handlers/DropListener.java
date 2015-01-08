03f047c5c926ef702e8f8936ab24481c3b2e55e0
PureIndianSkillz <patelravv17@yahoo.com>
2015-01-08 13:52:34 -0500

    Fixed no drop

--- src/PolytechOnline/handlers/DropListener.java
+++ src/PolytechOnline/handlers/DropListener.java
@@ -6,6 +6,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.SignChangeEvent;
+import org.bukkit.event.entity.EntityDeathEvent;
 import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.inventory.InventoryType;
@@ -23,31 +24,33 @@ public class DropListener implements Listener{
 	
 	
 	//Prevents player from throwing weapons//
-	public void onPlayerDropItem(PlayerDropItemEvent event){
-		if(event.getItemDrop().getItemStack().getType() == Material.BOWL);
-		if(event.getItemDrop().getItemStack().getType() == Material.STONE_SWORD);
-		if(event.getItemDrop().getItemStack().getType() == Material.WOOD_SWORD);
-		if(event.getItemDrop().getItemStack().getType() == Material.BOW);
-		if(event.getItemDrop().getItemStack().getType() == Material.ARROW);
-		if(event.getItemDrop().getItemStack().getType() == Material.STICK);//test//
-		{
-		event.setCancelled(true);}}
+	public void onPlayerDropItem(PlayerDropItemEvent event) {
 	
-	//Prevents player from taking off armor//
-	public  void onClick(InventoryClickEvent event){
-	        if(event.getSlotType() == InventoryType.SlotType.ARMOR){
-	         event.setCancelled(true);}}
-	  
-	//Prevents player from dropping items on death//
-	public void onPlayerDeath(PlayerDeathEvent event){
-	        	event.getDrops().clear();}
-	        
-	        
-		{
-			
-			
-			
-			
-		}}
+		if(event.getItemDrop().getItemStack().getType() == Material.BOWL){
+			event.setCancelled(true); }
+		if(event.getItemDrop().getItemStack().getType() == Material.STONE_SWORD){
+			event.setCancelled(true); }
+		if(event.getItemDrop().getItemStack().getType() == Material.WOOD_SWORD){
+			event.setCancelled(true); }
+		if(event.getItemDrop().getItemStack().getType() == Material.BOW){
+			event.setCancelled(true); }
+		if(event.getItemDrop().getItemStack().getType() == Material.ARROW){
+			event.setCancelled(true); }
+		if(event.getItemDrop().getItemStack().getType() == Material.STICK){
+			event.setCancelled(true); }}
+	
 
+	@EventHandler
+	//Prevents player from taking off armor//
+		public  void onClick(InventoryClickEvent event){
+		        if(event.getSlotType() == InventoryType.SlotType.ARMOR){
+		         event.setCancelled(true);}}
+		  
+		//Prevents player from dropping items on death//
+		public void onPlayerDeath(PlayerDeathEvent event){
+		        	event.getDrops().clear();}
+
+}
+		
+		
 	
